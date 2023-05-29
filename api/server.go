c4ad8f69429cd1ee3700e36d41dc99fa7fef926a
naveen <naveenkanuri@gmail.com>
2023-05-29 03:53:53 -0700

    Added test cases for api with mockgen

--- api/server.go
+++ api/server.go
@@ -7,30 +7,29 @@ import (
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-  store *db.Store
-  router *gin.Engine
+	store  db.Store
+	router *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(store *db.Store) *Server {
-  server := &Server{store: store}
-  router := gin.Default()
+func NewServer(store db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
 
-  // add routes to router
-  router.POST("/accounts", server.createAccount)
-  router.GET("/accounts/:id", server.getAccount)
-  router.GET("/accounts", server.listAccount)
+	// add routes to router
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
 
-  server.router = router
-  return server
-
+	server.router = router
+	return server
 }
 
 // Start runs the HTTP server on a specific address.
 func (server *Server) Start(address string) error {
-  return server.router.Run(address)
+	return server.router.Run(address)
 }
 
 func errorResponse(err error) gin.H {
-  return gin.H{"error": err.Error()}
+	return gin.H{"error": err.Error()}
 }
